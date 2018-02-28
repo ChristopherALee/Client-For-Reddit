b48024575fbb0da600002080db31099bc1e0e4a4
ChristopherALee <christopheradamlee@gmail.com>
2018-02-28 15:38:34 -0500

    fetches current subreddit on subreddit refresh

--- src/components/sub_reddits/sub_reddit/sub_reddit.jsx
+++ src/components/sub_reddits/sub_reddit/sub_reddit.jsx
@@ -6,11 +6,28 @@ class SubReddit extends React.Component {
     super(props);
   }
 
+  componentDidMount() {
+    this.props.fetchRedditAccessToken().then(success => {
+      this.props
+        .fetchSubRedditPosts(
+          this.props.accessToken,
+          this.props.currentSubReddit
+        )
+        .then(success => {
+          this.props.fetchSubRedditAbout(
+            this.props.accessToken,
+            this.props.currentSubReddit
+          );
+        });
+    });
+  }
+
   render() {
     return (
       <div id="sub-reddit-view">
         <div>subreddit</div>
         <div>subreddit details</div>
+        <div className="subreddit-preview">subreddit preview</div>
       </div>
     );
   }
--- src/components/sub_reddits/sub_reddit/sub_reddit_container.js
+++ src/components/sub_reddits/sub_reddit/sub_reddit_container.js
@@ -1,4 +1,41 @@
 import { connect } from "react-redux";
 import SubReddit from "./sub_reddit";
+import {
+  fetchRedditAccessToken,
+  fetchSubReddits,
+  fetchSubRedditAbout,
+  fetchSubRedditPosts
+} from "../../../actions/reddit_api_actions";
 
-export default connect(null, null)(SubReddit);
+const mapStateToProps = (state, ownProps) => {
+  let currentSubReddit = ownProps.location.pathname.slice(3);
+
+  let accessToken;
+  if (state.session) {
+    accessToken = state.session.token;
+  }
+
+  let subRedditDetails;
+  if (state.entities.subReddits[currentSubReddit]) {
+    subRedditDetails = state.entities.subReddits[currentSubReddit];
+  }
+
+  return {
+    accessToken: accessToken,
+    currentSubReddit: currentSubReddit,
+    subRedditDetails: subRedditDetails
+  };
+};
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+    fetchRedditAccessToken: () => dispatch(fetchRedditAccessToken()),
+    fetchSubReddits: (token, topic) => dispatch(fetchSubReddits(token, topic)),
+    fetchSubRedditPosts: (token, subReddit) =>
+      dispatch(fetchSubRedditPosts(token, subReddit)),
+    fetchSubRedditAbout: (token, subReddit) =>
+      dispatch(fetchSubRedditAbout(token, subReddit))
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(SubReddit);
